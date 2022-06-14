9b1e06473e49cfda702e7cf397ab70d63f0a801a
Sean <sean.saez.f@gmail.com>
2022-06-14 10:35:05 +0200

    Se le va sumando uno al array de numeros

--- src/components/Lista.js
+++ src/components/Lista.js
@@ -2,11 +2,16 @@ import React, {useState, Fragment} from 'react';
 
 const Lista = () => {
 
-    const [arrayNumero, setNumero] = useState([1,2,3,4,5])
+    const [arrayNumero, setArrayNumero] = useState([1,2,3,4,5])
+    // Puedes utilizar todos los useState que necesites
+    const [numero, setNumero] = useState(5)
 
+    
     const agregarElemento = () => {
         console.log('click')
-        setNumero([...arrayNumero, 6]) // Esto es para ampliar el array sin sobreescribirlo
+
+        setNumero(numero + 1)
+        setArrayNumero([...arrayNumero, numero]) // Esto es para ampliar el array sin sobreescribirlo
         // sin los tres puntos juntaria todos los numeros sin recorrerlos uno por uno
     }
 
@@ -18,6 +23,8 @@ const Lista = () => {
                 arrayNumero.map((item, index) => 
                     <p key={index}> {item} - {index}</p>
                 )
+                //Con este codigo recorres la array, el index siempre empieza por 0.
+                //item es el contenido del array, lo recorre uno por uno
             }
         </Fragment>
      );
